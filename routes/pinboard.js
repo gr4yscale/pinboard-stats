feb08b3ff1e43b42737ae4442ea0f21e8f787382
Tyler Powers <gr4yscale@gmail.com>
2015-05-22 13:49:17 +0100

    use params coming in from endpoint to filter time series data

--- routes/pinboard.js
+++ routes/pinboard.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/timeSeries', function(req, res, next) {
-	// temp mock data source; implement mongodb fetching next
-	res.sendFile('/Users/gr4yscale/code/pinboard-stats/theJSON.json');
+router.get('/timeSeries/:startDate/:endDate/:numTopTags', function(req, res, next) {
+
+	var startDate = new Date(req.params.startDate);
+	var endDate = new Date(req.params.endDate);
+
+	var pinboard_service = req.app.get("pinboard_service");
+	var currently_fetched_posts = pinboard_service.posts;
+	var filteredPosts = pinboard_service.postsForDateRange(currently_fetched_posts, startDate, endDate);
+	var timeSeries = pinboard_service.timeSeriesPacked(filteredPosts, startDate, endDate, parseInt(req.params.numTopTags));
+	res.json(timeSeries);
 });
 
 module.exports = router;
